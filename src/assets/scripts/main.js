52b07aa8b5e0168656c33d5fa2d5fd963e9d2caa
CTNeatDavid <david@clautronic.es>
2023-10-20 15:32:06 +0200

    mobile

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,61 +1,48 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//   const sections = document.querySelectorAll('.section');
-//   let currentSection = 0;
-
-//   sections[currentSection].style.transform = 'translateY(0)';
-
-
-//   window.addEventListener('wheel', (e) => {
-//       if (e.deltaY > 0 && currentSection < sections.length - 1) {
-//           currentSection++;
-//       } else if (e.deltaY < 0 && currentSection > 0) {
-//           currentSection--;
-//       }
-
-//       sections.forEach((section, index) => {
-//           section.style.transform = `translateY(-${100 * (currentSection)}vh)`;
-//       });
-//   });
-// });
-
+ 
 document.addEventListener('DOMContentLoaded', () => {
-  const sections = document.querySelectorAll('.section');
-  let currentSection = 0;
-  let isScrolling = false;
-  let startY = 0;
-
-  sections[currentSection].style.transform = 'translateY(0)';
-
-  window.addEventListener('wheel', scrollHandler);
-
-  window.addEventListener('touchstart', (e) => {
-      startY = e.touches[0].clientY;
-  });
-
-  window.addEventListener('touchmove', (e) => {
-      const deltaY = e.touches[0].clientY - startY;
-      scrollHandler({ deltaY });
-      startY = e.touches[0].clientY;
-  });
-
-  function scrollHandler(e) {
+    const sections = document.querySelectorAll('.section');
+    let currentSection = 0;
+    let isScrolling = false;
+    let startY = 0;
+  
+    sections[currentSection].style.transform = 'translateY(0)';
+  
+    // // Evento para el desplazamiento en dispositivos táctiles
+    // window.addEventListener('touchstart', (e) => {
+    //   startY = e.touches[0].clientY;
+    // });
+  
+    // window.addEventListener('touchmove', (e) => {
+    //   const deltaY = startY - e.touches[0].clientY; // Invertir la dirección del desplazamiento
+    //   scrollHandler({ deltaY });
+    //   startY = e.touches[0].clientY;
+    //   e.preventDefault(); // Evita el desplazamiento predeterminado en dispositivos táctiles
+    // });
+  
+    // Evento para el desplazamiento con la rueda del ratón en ordenadores
+    window.addEventListener('wheel', (e) => {
+      scrollHandler({ deltaY: e.deltaY });
+    });
+  
+    function scrollHandler(e) {
       if (!isScrolling) {
-          isScrolling = true;
-          setTimeout(() => {
-              isScrolling = false;
-          }, 800);
-
-          if (e.deltaY > 0 && currentSection < sections.length - 1) {
-              currentSection++;
-          } else if (e.deltaY < 0 && currentSection > 0) {
-              currentSection--;
-          }
-
-          sections.forEach((section, index) => {
-              section.style.transform = `translateY(-${100 * (currentSection)}vh)`;
-          });
+        isScrolling = true;
+        setTimeout(() => {
+          isScrolling = false;
+        }, 800);
+  
+        if (e.deltaY > 0 && currentSection < sections.length - 1) {
+          currentSection++;
+        } else if (e.deltaY < 0 && currentSection > 0) {
+          currentSection--;
+        }
+  
+        sections.forEach((section, index) => {
+          section.style.transform = `translateY(-${100 * currentSection}vh)`;
+        });
       }
-  }
-});
+    }
+  });
 
-
+  
+  
